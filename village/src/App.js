da2485958e046dabd1dd6c0354e1ec38e6f34b6a
Agustina Sosa <agustinasf85@gmail.com>
2019-05-24 12:17:23 -0400

    Constructed AXIOS request GET and POST. Started working on SmurfForm

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Route, } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -11,9 +13,20 @@ class App extends Component {
       smurfs: [],
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+
+  componentDidMount(){
+    axios.get('http://localhost:3333/smurfs')
+    .then(res => {
+      this.setState({ smurfs: Response.data })
+    })
+    .catch( err => console.log(err) );
+  }
+
+  // add any needed code to ensure that the smurfs collection exists
+  // on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  
   render() {
     return (
       <div className="App">
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { withRouter } from "react-router-dom";
+
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,6 +16,13 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+
+    axios.post('http://localhost:3333/smurfs', {...this.state})
+    .then(res => {
+      this.props.onSubmit(res.data);
+      this.props.history.push('/')
+    })
+    .catch(err => console.log(err));
 
     this.setState({
       name: '',
